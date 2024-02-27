81b536fafcf50b0d969477fb9653dd52edef4065
hanyangzhou-PS16 <130773491+hanyangzhou-PS16@users.noreply.github.com>
2024-02-26 19:40:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -39,6 +39,66 @@ function resizeCanvas() {
     canvas.height = window.innerHeight - 28;
 }
 
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawBall();
+
+    drawPaddle(player1.x, player1.y, player1.color);
+    drawPaddle(player2.x, player2.y, player2.color);
+
+    ball.x += ball.speedX;
+    ball.y += ball.speedY;
+
+    if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
+        ball.speedY = -ball.speedY;
+    }
+
+    if (
+        ball.x - ball.radius < player1.x + paddleWidth &&
+        ball.y > player1.y &&
+        ball.y < player1.y + paddleHeight
+    ) {
+        ball.speedX = -ball.speedX;
+        increaseBallSpeed();
+    }
+
+    if (
+        ball.x + ball.radius > player2.x &&
+        ball.y > player2.y &&
+        ball.y < player2.y + paddleHeight
+    ) {
+        ball.speedX = -ball.speedX;
+        increaseBallSpeed();
+    }
+
+    if (ball.x - ball.radius < 0) {
+        player2.score++;
+        document.getElementById('player2Score').textContent = player2.score;
+        resetBall();
+    } else if (ball.x + ball.radius > canvas.width) {
+        player1.score++;
+        document.getElementById('player1Score').textContent = player1.score;
+        resetBall();
+    }
+
+    if (keysPressed['w']) {
+        player1.y -= player1.speed;
+    }
+    if (keysPressed['s']) {
+        player1.y += player1.speed;
+    }
+    if (keysPressed['ArrowUp']) {
+        player2.y -= player2.speed;
+    }
+    if (keysPressed['ArrowDown']) {
+        player2.y += player2.speed;
+    }
+
+    player1.y = Math.max(0, Math.min(player1.y, canvas.height - paddleHeight));
+    player2.y = Math.max(0, Math.min(player2.y, canvas.height - paddleHeight));
+}
+
 window.addEventListener('resize', () => {
     resizeCanvas();
     draw();
@@ -84,66 +144,8 @@ function startGame() {
 
     const keysPressed = {};
 
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        drawBall();
-
-        drawPaddle(player1.x, player1.y, player1.color);
-        drawPaddle(player2.x, player2.y, player2.color);
-
-        ball.x += ball.speedX;
-        ball.y += ball.speedY;
-
-        if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
-            ball.speedY = -ball.speedY;
-        }
-
-        if (
-            ball.x - ball.radius < player1.x + paddleWidth &&
-            ball.y > player1.y &&
-            ball.y < player1.y + paddleHeight
-        ) {
-            ball.speedX = -ball.speedX;
-            increaseBallSpeed();
-        }
-
-        if (
-            ball.x + ball.radius > player2.x &&
-            ball.y > player2.y &&
-            ball.y < player2.y + paddleHeight
-        ) {
-            ball.speedX = -ball.speedX;
-            increaseBallSpeed();
-        }
-
-        if (ball.x - ball.radius < 0) {
-            player2.score++;
-            document.getElementById('player2Score').textContent = player2.score;
-            resetBall();
-        } else if (ball.x + ball.radius > canvas.width) {
-            player1.score++;
-            document.getElementById('player1Score').textContent = player1.score;
-            resetBall();
-        }
-
-        if (keysPressed['w']) {
-            player1.y -= player1.speed;
-        }
-        if (keysPressed['s']) {
-            player1.y += player1.speed;
-        }
-        if (keysPressed['ArrowUp']) {
-            player2.y -= player2.speed;
-        }
-        if (keysPressed['ArrowDown']) {
-            player2.y += player2.speed;
-        }
-
-        player1.y = Math.max(0, Math.min(player1.y, canvas.height - paddleHeight));
-        player2.y = Math.max(0, Math.min(player2.y, canvas.height - paddleHeight));
-    }
-
+    draw()
+    
     function increaseBallSpeed() {
         if (Math.abs(ball.speedX) < ball.maxSpeed) {
             ball.speedX += ball.speedIncrement * Math.sign(ball.speedX);
